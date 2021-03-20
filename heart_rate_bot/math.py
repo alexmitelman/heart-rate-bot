5a51b4fb519cebe3a09610b559c1e6ff5d0824a4
alexmitelman <alexmitelman@gmail.com>
2021-03-20 22:22:34 +0200

    Add basic logic

---
+++ heart_rate_bot/math.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from typing import Tuple
+
+
+def calculate_heart_rate(
+    age: int, reasting_heart_rate: int
+) -> Tuple[Decimal, Decimal]:
+    max_heart_rate = 220 - age
+    hrr = max_heart_rate - reasting_heart_rate
+    target_low = Decimal(hrr * 0.7 + reasting_heart_rate)
+    target_high = Decimal(hrr * 0.85 + reasting_heart_rate)
+    return target_low, target_high
--- tests/test_heart_rate_bot.py
+++ tests/test_heart_rate_bot.py
@@ -1,5 +1,11 @@
 from heart_rate_bot import __version__
+from heart_rate_bot.math import calculate_heart_rate
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_calculate_heart_rate():
+    result = calculate_heart_rate(45, 80)
+    assert result == (146.5, 160.75)
